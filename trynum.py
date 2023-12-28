65c553b7b07144461d5418e433ea25ef26d72526
Mohak JD <mohakjd@Mohaks-MacBook-Pro.local>
2023-12-29 02:18:53 +0530

    Task 3.1 Complete

--- trynum.py
+++ trynum.py
@@ -1,7 +1,7 @@
 # Written by : Mohak
 # Date : 29/12/2023
 # Time : 2:00AM
-# Notepad Ref 3 : Write a python Program which writes 1 to 6 in horizontal line with 6 spaces as well as their square as well as their square root upto 3 decimal places
+# Notepad Ref 3.1 : Write a python Program which writes 1 to 6 in horizontal line with 6 spaces as well as their square as well as their square root upto 2 decimal places.
 
 for i in range(1,7):
     print(i,end="      ")
@@ -12,5 +12,5 @@ for i in range(1,7):
 print()
 
 for i in range(1,7):
-    print(round(i**0.5,3),end="      ")
+    print(round(i**0.5,2),end="      ")
 print()
