be303fd1614e76474ca310012008e803ad51c89b
Mohak JD <mohakjd@Mohaks-MacBook-Pro.local>
2023-12-29 01:19:54 +0530

    Task 1 Complete

---
+++ trynum.py
@@ -0,0 +1,10 @@
+# Written by : Mohak
+# Date : 29/12/2023
+# Time : 1:18AM
+# Notepad Ref 1 : Write a python Program which writes 1 to 6 in horizontal line with 6 spaces.
+
+for i in range(1,7):
+    print(i,end="      ")
+print()
+
+
