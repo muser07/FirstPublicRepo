3d67e1fdfeddba22d8b8e0eace67e72764188afa
Mohak JD <mohakjd@Mohaks-MacBook-Pro.local>
2023-12-29 02:37:43 +0530

    Task 7 Complete

--- trynum.py
+++ trynum.py
@@ -1,21 +1,24 @@
 # Written by : Mohak
 # Date : 29/12/2023
 # Time : 2:00AM
-# Notepad Ref 6 : Modify the order :  number, square root, square , cube
+# Notepad Ref 7 : Modify to mention : number, square root, square , cube
 
-
+print("Number : ",end=" ")
 for i in range(1,7):
     print(i,end="      ")
 print()
 
+print("Square Root : ",end=" ")
 for i in range(1,7):
     print(round(i**0.5,3),end="      ")
 print()
 
+print("Square : ",end=" ")
 for i in range(1,7):
     print(i**2,end="      ")
 print()
 
+print("Cube : ",end=" ")
 for i in range(1,7):
     print(i**3,end="      ")
 print()
