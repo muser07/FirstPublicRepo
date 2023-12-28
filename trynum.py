93dba1b0b76303147b29a0b592b902f023b45990
Mohak JD <mohakjd@Mohaks-MacBook-Pro.local>
2023-12-29 01:23:44 +0530

    Task 2 Complete

--- trynum.py
+++ trynum.py
@@ -1,10 +1,13 @@
 # Written by : Mohak
 # Date : 29/12/2023
-# Time : 1:18AM
-# Notepad Ref 1 : Write a python Program which writes 1 to 6 in horizontal line with 6 spaces.
+# Time : 1:20AM
+# Notepad Ref 2 : Write a python Program which writes 1 to 6 in horizontal line with 6 spaces as well as their square
 
 for i in range(1,7):
     print(i,end="      ")
 print()
 
+for i in range(1,7):
+    print(i**2,end="      ")
+print()
 
